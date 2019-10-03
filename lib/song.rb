6d625473c1b3abf0d62d5d1de7afd6d504d6a606
sarah <s.j.condon01@gmail.com>
2019-10-03 02:36:45 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,50 @@
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@genres = []
+  @@artists = []
+  
+  def initialize(name, artist,genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@genres << genre
+    @@artists << artist
+    @@count += 1
+  end
+  
+  def self.count
+    @@count
+  end
+  
+   def self.artists
+    @@artists.uniq
+  end
+  
+   def self.genres
+    @@genres.uniq
+  end
+  
+  def self.genre_count
+    hash={}
+    @@genres.each do |genre|
+      if !hash[genre]
+      hash[genre] = 1
+    else 
+  end
+  end
+  
+  # def name
+  #   @@name
+  # end
+  
+  # def artist
+  #   @@artist
+  # end
+  
+  # def genre
+  #   @@genre
+  # end
+
+end                 
